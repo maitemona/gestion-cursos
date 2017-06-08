5c1cb26d36da071eebf828d194ef6779130f0ef9
Maite Monasterio <maitemonasterio@gmail.com>
2017-06-08 16:41:07 +0200

    subida restante

---
+++ gestion-cursos/src/main/java/com/ipartek/formacion/controller/HomeController.java
@@ -0,0 +1,49 @@
+package com.ipartek.formacion.controller;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+@RequestMapping(value = "/")
+public class HomeController {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(HomeController.class);
+
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping(method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		LOGGER.info("Welcome home! The client locale is {}.", locale);
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+
+		String formattedDate = dateFormat.format(date);
+
+		model.addAttribute("serverTime", formattedDate);
+
+		return "home";
+	}
+
+	
+
+}
---
+++ gestion-cursos/src/main/java/com/ipartek/formacion/dbms/package-info.java
@@ -0,0 +1,9 @@
+
+/**
+ *  Este paquete contendrá las clases que determinan el acceso
+ * a base de datos: patrón DAO, capa de Persistencia y Mapeo de 
+ * clases para la gestión de Spring JDBC.
+ * @author Maite Monasterio
+ *
+ */
+package com.ipartek.formacion.dbms;
